7d05a806bbc0ca493b3be11391855047b5f1ff9c
INSTAY LLC <github@instay.io>
2025-07-20 16:53:17 -0500

    BIG branding

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -51,14 +51,14 @@ export const Badge = (props: Props) => {
           {props.footer?.text ?? 'Powered by'}
           <a
             ref={liteBadge}
-            href={props.footer?.companyLink ?? 'https://flowiseai.com'}
+            href={props.footer?.companyLink ?? 'https://ww2.georgiasouthern.edu/research/big/'}
             target="_blank"
             rel="noopener noreferrer"
             class="lite-badge"
             id="lite-badge"
             style={{ 'font-weight': 'bold', color: props.footer?.textColor ?? props.poweredByTextColor ?? defaultTextColor }}
           >
-            <span>&nbsp;{props.footer?.company ?? 'Flowise'}</span>
+            <span>&nbsp;{props.footer?.company ?? 'BIG'}</span>
           </a>
         </span>
       </Show>
